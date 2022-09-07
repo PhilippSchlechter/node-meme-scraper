4e6fea7beb89aae5e75a08736413cbe490f5a1a4
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-07 16:27:14 +0200

    Draft with esl packages fetc

---
+++ index.js
@@ -0,0 +1,8 @@
+import fetch from 'node-fetch';
+
+// fetch html from meme-website
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const body = await response.text();
+console.log(body);
