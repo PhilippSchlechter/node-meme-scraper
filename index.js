9320ab7714e1b70345d5659afef39c8ef45a6b95
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-08 18:59:26 +0200

    Fetch and cheerio and array

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
 // fetch html from meme-website
@@ -6,3 +7,20 @@ const response = await fetch(
 );
 const body = await response.text();
 //console.log(body);
+
+const $ = cheerio.load(body);
+//console.log($.html());
+const imageUrl = [];
+const imgSrc = $('img', body).each(function () {
+  const image = $(this).attr('src');
+  imageUrl.push(image);
+});
+
+//console.log(imageUrl);
+const first10Img = [];
+
+for (let i = 0; i < 10; i++) {
+  first10Img.push(imageUrl[i]);
+}
+
+console.log(first10Img[1]);
